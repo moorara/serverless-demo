3c0d974983c34d09bad856826d66e81af6484a7e
Milad Irannejad <moorara@gmail.com>
2018-04-29 13:31:26 -0400

    Add API path

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -3,58 +3,79 @@ resource "aws_api_gateway_rest_api" "serverless" {
   description = "api gateway for serverless application"
 }
 
-resource "aws_api_gateway_resource" "proxy" {
-  rest_api_id = "${aws_api_gateway_rest_api.serverless.id}"
-  parent_id   = "${aws_api_gateway_rest_api.serverless.root_resource_id}"
-  path_part   = "{proxy+}"
-}
+########################################  /  ########################################
 
-resource "aws_api_gateway_method" "proxy" {
+resource "aws_api_gateway_method" "root" {
   rest_api_id   = "${aws_api_gateway_rest_api.serverless.id}"
-  resource_id   = "${aws_api_gateway_resource.proxy.id}"
-  http_method   = "ANY"
+  resource_id   = "${aws_api_gateway_rest_api.serverless.root_resource_id}"
+  http_method   = "GET"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_method" "proxy_root" {
+resource "aws_api_gateway_integration" "root" {
+  rest_api_id             = "${aws_api_gateway_rest_api.serverless.id}"
+  resource_id             = "${aws_api_gateway_method.root.resource_id}"
+  http_method             = "${aws_api_gateway_method.root.http_method}"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.hello-world.invoke_arn}"
+}
+
+########################################  /api/v1  ########################################
+
+resource "aws_api_gateway_resource" "api" {
+  rest_api_id = "${aws_api_gateway_rest_api.serverless.id}"
+  parent_id   = "${aws_api_gateway_rest_api.serverless.root_resource_id}"
+  path_part   = "api"
+}
+
+resource "aws_api_gateway_resource" "v1" {
+  rest_api_id = "${aws_api_gateway_rest_api.serverless.id}"
+  parent_id   = "${aws_api_gateway_resource.api.id}"
+  path_part   = "v1"
+}
+
+########################################  /api/v1/hello-world  ########################################
+
+resource "aws_api_gateway_resource" "hello-world" {
+  rest_api_id = "${aws_api_gateway_rest_api.serverless.id}"
+  parent_id   = "${aws_api_gateway_resource.v1.id}"
+  path_part   = "hello-world"
+}
+
+resource "aws_api_gateway_method" "hello-world" {
   rest_api_id   = "${aws_api_gateway_rest_api.serverless.id}"
-  resource_id   = "${aws_api_gateway_rest_api.serverless.root_resource_id}"
-  http_method   = "ANY"
+  resource_id   = "${aws_api_gateway_resource.hello-world.id}"
+  http_method   = "GET"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "hello-world" {
   rest_api_id             = "${aws_api_gateway_rest_api.serverless.id}"
-  resource_id             = "${aws_api_gateway_resource.proxy.id}"
-  http_method             = "${aws_api_gateway_method.proxy.http_method}"
+  resource_id             = "${aws_api_gateway_resource.hello-world.id}"
+  http_method             = "${aws_api_gateway_method.hello-world.http_method}"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "${aws_lambda_function.hello-world.invoke_arn}"
 }
 
-resource "aws_api_gateway_integration" "hello-world_root" {
-  rest_api_id             = "${aws_api_gateway_rest_api.serverless.id}"
-  resource_id             = "${aws_api_gateway_method.proxy_root.resource_id}"
-  http_method             = "${aws_api_gateway_method.proxy_root.http_method}"
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.hello-world.invoke_arn}"
-}
-
-resource "aws_api_gateway_deployment" "serverless" {
-  depends_on = [
-    "aws_api_gateway_integration.hello-world",
-    "aws_api_gateway_integration.hello-world_root",
-  ]
-
-  rest_api_id = "${aws_api_gateway_rest_api.serverless.id}"
-  stage_name  = "staging"
-}
-
-resource "aws_lambda_permission" "api-gateway" {
-  statement_id  = "AllowAPIGatewayInvoke"  # AllowExecutionFromAPIGateway
+resource "aws_lambda_permission" "hello-world" {
+  # AllowExecutionFromAPIGateway
+  statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
   principal     = "apigateway.amazonaws.com"
   function_name = "${aws_lambda_function.hello-world.arn}"
   source_arn    = "${aws_api_gateway_deployment.serverless.execution_arn}/*/*"
+}
+
+####################################################################################################
+
+resource "aws_api_gateway_deployment" "serverless" {
+  depends_on = [
+    "aws_api_gateway_integration.root",
+    "aws_api_gateway_integration.hello-world",
+  ]
+
+  rest_api_id = "${aws_api_gateway_rest_api.serverless.id}"
+  stage_name  = "staging"
 }
