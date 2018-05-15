62cf6fc8417734d77d28411d1f225ec364c281f2
Milad Irannejad <moorara@gmail.com>
2018-05-15 00:46:13 -0400

    Add static-website module to Terraform code

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,6 +34,8 @@ module "api_gateway" {
   certificate_arn = "${module.certificate.certificate_arn}"
 }
 
+##################################################  ENDPOINTS  ##################################################
+
 module "func_message" {
   source = "./function"
 
@@ -53,4 +55,44 @@ module "endpoint_message" {
   parent_id            = "${module.api_gateway.v1_resource_id}"
   function_arns        = ["${module.func_message.arn}"]
   function_invoke_arns = ["${module.func_message.invoke_arn}"]
+}
+
+##################################################  WEBSITE  ##################################################
+
+module "webapp" {
+  source = "./static-website"
+
+  domain          = "${local.domain}"
+  environment     = "${var.environment}"
+  region          = "${var.region}"
+  bucket_name     = "${local.env_domain}"
+  index_page      = "index.html"
+  error_page      = "error.html"
+  zone_id         = "${data.aws_route53_zone.primary.id}"
+  certificate_arn = "${module.certificate.certificate_arn}"
+}
+
+# Build client app production bundle
+resource "null_resource" "build_client" {
+  depends_on = ["module.webapp"]
+
+  provisioner "local-exec" {
+    working_dir = "${path.module}/../client"
+    command     = "yarn run build 1> /dev/null"
+  }
+}
+
+# Upload client app bundle to S3 bucket
+resource "null_resource" "upload_client" {
+  depends_on = ["null_resource.build_client"]
+
+  provisioner "local-exec" {
+    working_dir = "${path.module}/../client/build"
+    command     = "aws s3 sync . s3://${module.webapp.bucket}"
+
+    environment {
+      AWS_ACCESS_KEY_ID     = "${var.access_key}"
+      AWS_SECRET_ACCESS_KEY = "${var.secret_key}"
+    }
+  }
 }
---
+++ terraform/static-website/main.tf
@@ -0,0 +1,96 @@
+resource "aws_s3_bucket" "main" {
+  bucket = "${var.bucket_name}"
+  region = "${var.region}"
+  acl    = "public-read"
+
+  website {
+    index_document = "${var.index_page}"
+    error_document = "${var.error_page}"
+  }
+
+  tags {
+    Environment = "${var.environment}"
+    Region      = "${var.region}"
+  }
+
+  policy = <<JSON
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${var.bucket_name}/*"
+    }
+  ]
+}
+JSON
+}
+
+resource "aws_cloudfront_distribution" "main" {
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "${var.index_page}"
+  aliases             = ["${var.domain}"]
+
+  # This for serving the single-page app from any path
+  custom_error_response {
+    error_code         = "404"
+    response_code      = "200"
+    response_page_path = "/${var.index_page}"
+  }
+
+  origin {
+    origin_id   = "${aws_s3_bucket.main.bucket}"
+    domain_name = "${aws_s3_bucket.main.bucket_domain_name}"
+  }
+
+  default_cache_behavior {
+    target_origin_id       = "${aws_s3_bucket.main.bucket}"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+
+    compress    = true
+    min_ttl     = "${lookup(var.cache, "min_ttl")}"
+    default_ttl = "${lookup(var.cache, "default_ttl")}"
+    max_ttl     = "${lookup(var.cache, "max_ttl")}"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = "${var.certificate_arn}"
+    ssl_support_method  = "sni-only"
+  }
+
+  tags {
+    Environment = "${var.environment}"
+    Region      = "${var.region}"
+  }
+}
+
+resource "aws_route53_record" "webapp" {
+  zone_id = "${var.zone_id}"
+  name    = "${var.domain}"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_cloudfront_distribution.main.domain_name}"
+    zone_id                = "${aws_cloudfront_distribution.main.hosted_zone_id}"
+    evaluate_target_health = true
+  }
+}
