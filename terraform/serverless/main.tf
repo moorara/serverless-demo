2e75dec544734b874a5bcd87d3cd095df321d7ef
Milad Irannejad <moorara@users.noreply.github.com>
2020-07-31 23:32:05 -0400

    Update packages and Terraform AWS provider

--- terraform/serverless/main.tf
+++ terraform/serverless/main.tf
@@ -8,8 +8,8 @@ terraform {
 # https://www.terraform.io/docs/providers/aws
 # https://www.terraform.io/docs/configuration/providers.html#version-provider-versions
 provider "aws" {
-  # Equivalent to ">= 2.34.0, < 2.0.0"
-  version    = "~> 2.34"  
+  # Equivalent to ">= 3.0.0, < 4.0.0"
+  version    = "~> 3.0"
   access_key = var.access_key
   secret_key = var.secret_key
   region     = var.region
